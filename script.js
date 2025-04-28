d6aacd6b5c53ce266b9c736ab3215026adafb58c
Fun <161741261+funfunido@users.noreply.github.com>
2025-04-28 10:26:16 -0600

    debug

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let stick = document.getElementById("stick-image");
+
 const img = document.querySelector("img");
 const images = document.getElementsByClassName("image");
 images.ondragstart = () => {
   return false;
 };
 
+
 function updateNames() {
     let theValue = document.getElementById("nameBox").value;
     alert(theValue)
+
+}
+
+$(document).keydown(function(event) {
+  if (event.key === 'Space') {
+    let names = document.getElementById('nameBox').value;  
+    spin(names);
+  }
+});
+
+function spin(names) {
+    alert(names)
 
 }
 
